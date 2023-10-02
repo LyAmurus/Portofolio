58fdabb684b7453a2a2b34d72ba1e527a464e016
Mulyadi <88414150+Lychannnnnn@users.noreply.github.com>
2023-10-02 13:52:40 +0700

    Penghapusan beberapa page

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,7 +26,7 @@ let header = $(`
    <li class="nav-item nav-item-hover"><a class="nav-link" href="index.html">Home</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="experience.html">Experience</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Research</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="techstack.html">Skill</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="design.html">Gallery</a></li>
    <li class="nav-item">
