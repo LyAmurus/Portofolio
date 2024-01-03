d84b83c13c1152fdcd5c34462edcae94ee7ee2de
Mulyadi <88414150+Lychannnnnn@users.noreply.github.com>
2024-01-03 18:51:10 +0700

    Penambahan Asset

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -14,16 +14,16 @@ const exp = [
   {
     title: "Google Developer Student Clubs",
     cardImage: "assets/images/experience-page/GDSC.png",
-    place: "Google Summer Of Code",
-    time: "(Mar - Aug, 2019)",
+    place: "Google Developer Of Code",
+    time: "(okt - Present)",
     desp: "<li>Worked with MOZILLA as a part of Google Summer Of Code.</li><li>Worked on Extension Activity Monitor which is a privileged extension for Firefox that uses the activityLog API (privileged API) to monitor the activities of the other installed extensions.</li><li>The activityLog API listens for logs from other installed extensions.</li>",
   },
   {
-    title: "Research Intern",
-    cardImage: "assets/images/experience-page/IIT_Bombay.jpg",
-    place: "IIT, Bombay",
-    time: "(Dec, 19 - Jan, 20)",
-    desp: "<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
+    title: "Student Hacktiv8 (MSIB)",
+    cardImage: "assets/images/experience-page/Hack.jpeg",
+    place: "Hacktivate Teknologi Indonesia",
+    time: "(aug, 14 - Dec, 31)",
+    desp: "<li>Conducted several planned projects, assigments, challenges. The final project was a car wash booking application, school web site, and health application.</li>",
   },
 ];
 
